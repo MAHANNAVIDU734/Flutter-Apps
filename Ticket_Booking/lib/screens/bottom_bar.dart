ddf73f5a0c13756120657f699877d6e8d4e3cf91
MAHANNAIDU734 <mahan.navidu@gmail.com>
2022-12-13 09:21:48 +0530

    bottom Navigation bar

--- Ticket_Booking/lib/screens/bottom_bar.dart
+++ Ticket_Booking/lib/screens/bottom_bar.dart
@@ -13,12 +13,25 @@ class _BottomBarState extends State<BottomBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Tickets"),
+        title: const Text("My Tickets"),
       ),
-      body: Center(
+      body: const Center(
         child: Text(
          "My Body"
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Ticket"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
       ),
     );
   }
